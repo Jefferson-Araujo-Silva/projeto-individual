7a7777c7b1639e04f8834b3aa7435ef3436ca97b
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2022-11-16 21:51:41 -0300

    conectando grafico com api

--- Site estático/srcjs/models/medidaModel.js
+++ Site estático/srcjs/models/medidaModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(Idfranquia, limite_linhas) {
 
     instrucaoSql = ''
 
@@ -14,14 +14,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     where fk_aquario = ${idAquario}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select time as franquia, count(fkFranquia) as qtd from usuario join franquia on idFranquia = fkFranquia group by fkFranquia order by qtd desc limit ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -31,7 +24,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(Idfranquia) {
 
     instrucaoSql = ''
 
@@ -45,13 +38,7 @@ function buscarMedidasEmTempoReal(idAquario) {
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = `select time as franquia, count(fkFranquia) as qtd from usuario join franquia on idFranquia = fkFranquia group by fkFranquia order by qtd desc limit 3`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
--- Site estático/srcjs/routes/medidas.js
+++ Site estático/srcjs/routes/medidas.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
+// router.get("/ultimas/:idAquario", function (req, res) {
+//     medidaController.buscarUltimasMedidas(req, res);
+// });
+
+// router.get("/tempo-real/:idAquario", function (req, res) {
+//     medidaController.buscarMedidasEmTempoReal(req, res);
+// })
+
+router.get("/ultimas/:Idfranquia", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
+
+router.get("/tempo-real/:Idfranquia", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
 
